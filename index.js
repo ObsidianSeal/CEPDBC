9b9ff645162571fc20413f2030176dbb3f651593
ObsidianSeal <bmgosselin@gmail.com>
2025-07-09 19:43:04 -0400

    planners also welcome

--- index.js
+++ index.js
@@ -76,7 +76,9 @@ server.on("status", async (server) => {
 			if (server.status == server.STATUS.OFFLINE) {
 				let account = await clientE.getAccount();
 				await server.setMOTD(
-					`§3§lCivil Engineers’ Paradise§7  |  §a§l${server.software.version}§7  |  §d§l${Math.round(account.credits * 100) / 100} ☰\n§6> > >§e skull emoji`
+					`§3§lCivil Engineers’ Paradise§7  |  §a§l${server.software.version}§7  |  §d§l${
+						Math.round(account.credits * 100) / 100
+					} ☰\n§6> > >§e [urban] planners also welcome! (HELLO!!!!)`
 				);
 
 				/*
